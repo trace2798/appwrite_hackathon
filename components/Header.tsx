3bd7e1b92a4a78cb52296f681e24b94ec44ba6a8
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-06-01 20:06:50 +0530

    added animation on main heading

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,12 +4,12 @@ const Header = () => {
   return (
     <>
       <header>
-        <div className="flex flex-col md:flex-row items-center justify-center p-5 bg-gray-500/10 rounded-b-2xl">
-          <h1 className=" pb-10 md:pb-0 object-contain font-bold text-4xl">
+        <div className="flex flex-col md:flex-row items-center justify-between p-5 bg-gray-500/10 rounded-b-2xl">
+          <h1 className="text-4xl lg:text-8xl font-semibold bg-gradient-to-r bg-clip-text text-transparent from-yellow-500 via-pink-500 to-orange-500 dark:from-yellow-500 dark:via-purple-500 dark:to-red-500 animate-text">
             Task Master
           </h1>
-          <ThemeToggle/>
-          <div className="absolute top-0 left-0 w-full h-96 bg-gradient-to-br from-pink-400 to-blue-500 filter blur-3xl opacity-50 -z-50" />
+          <ThemeToggle />
+          {/* <div className="absolute top-0 left-0 w-full h-96 bg-gradient-to-br from-pink-400 to-blue-500 filter blur-3xl opacity-50 -z-50" /> */}
         </div>
         <div className="flex items-center justify-center px-5 md:py-5">
           <p className="flex items-center text-xl font-semibold  w-fit  max-w-3xl text-[#005501]">
