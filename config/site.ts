f80b6618574c87c8894fa6b4c84c26944e0cccd9
trace2798 <113078518+trace2798@users.noreply.github.com>
2023-07-10 13:34:44 +0530

    change in deployed link

--- config/site.ts
+++ config/site.ts
@@ -3,8 +3,8 @@ export const siteConfig: SiteConfig = {
   name: "Task Manager",
   description:
     "An open source application to help you manage your day-to-day task built using Next.js 13.4 for appwrite hashnode hackathon.",
-  url: "https://appwrite-hashnode-hackathon.vercel.app/",
-  ogImage: "https://appwrite-hashnode-hackathon.vercel.app/og.jpg",
+  url: "https://taskmaster-test.vercel.app",
+  ogImage: "https://taskmaster-test.vercel.app/og.jpg",
   links: {
     twitter: "https://twitter.com/Tisonthemove247",
     github: "https://github.com/trace2798/appwrite_hackathon",
